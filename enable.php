b70c9006b11cb8d18c0843ad9e06948d1cd53a91
Juthawong Naisanguansee <juthawong@users.noreply.github.com>
2015-12-18 12:34:45 +0700

    Update enable.php

--- enable.php
+++ enable.php
@@ -1,9 +1,9 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-if(session_id() == ''){
-     session_start(); 
-}
-$window = $_SESSION['window'];
-$_SESSION['jsenable'.$window] = TRUE;
+	if(session_id() == ''){
+		session_start(); 
+	}
+	$window = $_SESSION['window'];
+	$_SESSION['jsenable'.$window] = TRUE;
 }
 ?>
